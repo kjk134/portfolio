f21a00bd954ca362358a8824ef1d98920e172bc5
Sean Lawrence <sean.w.lawrence@gmail.com>
2017-10-15 23:19:37 -0500

    inital working prototype

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import Navigation from './partials/navigation/Navigation'
 import Contact from './pages/contact/Contact'
 import Work from './pages/work/Work'
-import Services from './pages/services/Services'
+//import Services from './pages/services/Services'
 import About from './pages/about/About'
 import Blog from './pages/blog/Blog'
 import {
@@ -19,7 +19,6 @@ class App extends Component {
                 <Navigation />
                   <Route exact path="/" component={Contact} />
                   <Route exact path="/about" component={About} />
-                  <Route exact path="/services" component={Services} />
                   <Route exact path="/work" component={Work} />
                   <Route exact path="/blog" component={Blog} />
             </div>
